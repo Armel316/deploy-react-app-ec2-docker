53716f5209911bcf3aaf975a8a7e6cfdcd6f8419
Franc Armel <armel.rakotonirina@yas.mg>
2026-02-24 15:37:06 +0300

    debug app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
+
+// Cette ligne empêche axios de faire de vraies requêtes réseau
+jest.mock('axios', () => ({
+  get: jest.fn(() => Promise.resolve({ data: [] })),
+  post: jest.fn(() => Promise.resolve({ data: [] }))
+}));
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
