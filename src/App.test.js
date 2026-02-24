daa5565e897394704260dae30e3f6b08cb3ef9b0
Franc Armel <armel.rakotonirina@yas.mg>
2026-02-24 15:26:45 +0300

    debug app test

--- src/App.test.js
+++ src/App.test.js
@@ -2,22 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-jest.mock('axios', () => ({
-  __esModule: true,
-  default: {
-    get: jest.fn().mockResolvedValue({
-      data: [
-        {
-          id: 1,
-          name: "John Doe",
-          email: "john@test.com",
-          phone: "123456"
-        }
-      ]
-    })
-  }
-}));
-
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
