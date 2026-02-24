5a123564a47e49905d83e87bcf2d4f4ec9a378a0
Franc Armel <armel.rakotonirina@yas.mg>
2026-02-24 14:57:56 +0300

    Update App Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
 
-it('renders without crashing', () => {
+jest.mock('axios');
+
+it('renders without crashing', async () => {
+  axios.get.mockResolvedValue({
+    data: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "john@test.com",
+        phone: "123456"
+      }
+    ]
+  });
+
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
