756c9a1d2acbbafbca7c1682d71365cf8477bdd1
Franc Armel <armel.rakotonirina@yas.mg>
2026-02-24 15:04:22 +0300

    debug app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import axios from 'axios';
 
 jest.mock('axios', () => ({
-  get: jest.fn()
+  __esModule: true,
+  default: {
+    get: jest.fn().mockResolvedValue({
+      data: [
+        {
+          id: 1,
+          name: "John Doe",
+          email: "john@test.com",
+          phone: "123456"
+        }
+      ]
+    })
+  }
 }));
 
-it('renders without crashing', async () => {
-  axios.get.mockResolvedValue({
-    data: [
-      {
-        id: 1,
-        name: "John Doe",
-        email: "john@test.com",
-        phone: "123456"
-      }
-    ]
-  });
-
+it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
